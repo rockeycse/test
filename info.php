65769344f20a09f49fce2ec1c96a83b652e26814
Rockey Ahmed <rockeycse@gmail.com>
2022-02-08 13:16:47 +0600

    I like chilly

---
+++ info.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "Hello world";
+    $yourGender= "Male";
+    echo "<br>";
+    if($yourGender == "Male"){
+        echo "you are male";
+    }else{
+        echo "you are female";
+    }
+    echo "<br> I like water";
+    echo "<br> I like rice";
+    echo "<br> I like chilly";
+    
+?>
